54005d367fd03dc756d049175a9bbbdda1c4ebab
Nicolas Ferro <nicoferro91@gmail.com>
2022-04-04 19:37:39 -0300

    division de agregarPedido a funciones mas chicas

--- codigo.js
+++ codigo.js
@@ -24,31 +24,62 @@ alert("Nuestro menu: "+JSON.stringify(menu, null, 4)+"Envio: 50");
 
 let importe = 0;
 let pedidoTotal = [];
+let producto = "a";
+let cantidad = 0;
 function agregarPedido()
 {
-    const nuevoPedido = new Pedido(id,prompt("Que queres pedir?"),parseInt(prompt("ingrese cantidad")));
-    if(!(nuevoPedido.nombre in menu))
+    producto = elegirProducto();
+    console.log(producto);
+    cantidad = elegirCantidad();
+    console.log(cantidad);
+    const nuevoPedido = new Pedido(id,producto,cantidad);
+    id++;
+    pedidoTotal.push(nuevoPedido);
+    importe=importe+nuevoPedido.precio;
+}
+
+function elegirProducto()
+{
+    producto = prompt("Elegi un producto");
+    console.log("en funcion " + producto);
+    if(!(producto in menu))
     {
         alert("El producto ingresado no existe");
+        return elegirProducto();        
     }
     else
     {
-        if(nuevoPedido.cantidad < 1)
-        {
-            alert("Por favor ingrese una cantidad mayor a cero")
-        }
-        else if(nuevoPedido.cantidad > 50)
-        {
-            alert("Parece que te equivocaste con la cantidad")
-        }
-        else
-        {
-            id++;
-            pedidoTotal.push(nuevoPedido);
-            importe=importe+nuevoPedido.precio;
-        }
+        console.log("en return " + producto)
+        return producto;
     }
 }
+
+function elegirCantidad()
+{
+    cantidad = parseInt(prompt("Elegi la cantidad"));
+    console.log("en funcion " + cantidad);
+    if(isNaN(cantidad))
+    {
+        alert("Por favor ingrese un numero");
+        return elegirCantidad(cantidad);
+    }
+    else if(cantidad < 1)
+    {
+        alert("Por favor ingrese una cantidad mayor a cero");
+        return elegirCantidad(cantidad);
+    }
+    else if(cantidad > 50)
+    {
+        alert("Por favor menos de 50");
+        return elegirCantidad(cantidad);
+    }
+    else
+    {
+        console.log("en return " + cantidad)
+        return cantidad;
+    }
+}
+
 
 let pedir = true;
 while(pedir)
@@ -74,3 +105,35 @@ else
 }
 importe=importe*descuento+envio;
 alert("Tu pedido: "+JSON.stringify(pedidoTotal, null, 4)+"Tu importe final: "+importe+" Tu pedido esta en camino");
+
+
+
+
+//viejo
+/*
+function agregarPedido()
+{
+    const nuevoPedido = new Pedido(id,prompt("Que queres pedir?"),parseInt(prompt("ingrese cantidad")));
+    if(!(nuevoPedido.nombre in menu))
+    {
+        alert("El producto ingresado no existe");
+    }
+    else
+    {
+        if(nuevoPedido.cantidad < 1)
+        {
+            alert("Por favor ingrese una cantidad mayor a cero")
+        }
+        else if(nuevoPedido.cantidad > 50)
+        {
+            alert("Parece que te equivocaste con la cantidad")
+        }
+        else
+        {
+            id++;
+            pedidoTotal.push(nuevoPedido);
+            importe=importe+nuevoPedido.precio;
+        }
+    }
+}
+*/
