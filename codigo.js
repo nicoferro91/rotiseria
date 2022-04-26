7297adab109e7f1cabc9cf9bcfd3100573b20072
Nicolas Ferro <nicoferro91@gmail.com>
2022-04-25 21:55:46 -0300

    agregado carrito

--- codigo.js
+++ codigo.js
@@ -52,6 +52,14 @@ const menuBase =
     Agua: 150,
 }
 
+class Agregado {
+    constructor(nombre, cantidad)
+    {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+    }
+}
+
 sessionStorage.setItem("MI_MENU",JSON.stringify(menuBase));
 let menu = JSON.parse(sessionStorage.getItem("MI_MENU"));
 
@@ -59,6 +67,92 @@ let menu = JSON.parse(sessionStorage.getItem("MI_MENU"));
 displayMenu(menu);
 
 let id = 1;
+/*
+function chequeoCompra()
+{
+    let carrito = document.getElementById("carrito")
+    let contador = 0;
+    for(let producto in menu)
+    {
+        let item = document.getElementById("cant"+contador)
+        item.onchange = ()=> {
+            let agregado = document.createElement("li")
+            agregado.innerText = producto + " " + item.value
+            carrito.append(agregado)
+
+        }
+        contador++;
+    }
+}
+
+class Agregado {
+    constructor(id, nombre, cantidad){
+        this.id = id;
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+    }
+}
+*/
+let compras = []
+let i = 0;
+function chequeoCompra()
+{
+    let i = 0;
+    for(let producto in menu)
+    {
+        let item = document.getElementById("cant"+i)
+        item.onchange = ()=> {
+            let sumado = new Agregado(producto,item.value)
+
+            if(compras.length==0) 
+            {
+                compras.push(sumado)
+            }
+            else
+            {
+                let falta=0;
+                for(let agregados in compras)
+                {
+                    let nuevo = Object.values(compras[agregados])
+                    nuevo = nuevo[0]
+                    if(producto==nuevo)
+                    {
+                        compras[agregados] = sumado;
+                        falta=0;
+                    }
+                    else
+                    {
+                        falta=1;
+                    }
+                }
+                if(falta==1)
+                {
+                    compras.push(sumado)
+                }
+            }
+            console.log(compras)
+            updateCarrito(compras)
+        }
+        i++;
+    }
+}
+chequeoCompra()
+
+function updateCarrito(compras)
+{
+    let carrito = document.getElementById("carrito")
+    while(carrito.firstChild)
+    {
+        carrito.firstChild.remove()
+    }
+    for(let comprados in compras)
+    {
+        let comprado = document.createElement("li");
+        comprado.innerText = Object.values(compras[comprados])[0] + ": " + Object.values(compras[comprados])[1]
+        carrito.append(comprado)
+    }
+}
+
 
 function hacerPedido()
 {
@@ -96,5 +190,7 @@ function contarCantidades()
     }
     return cantidades;
 }
+
+
 
 hacerPedido();
